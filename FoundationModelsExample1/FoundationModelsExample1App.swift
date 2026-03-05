2b525c9d43182d4fd5281056742942fc9340e98e
samtari1 <samtariyang@gmail.com>
2026-03-05 09:36:50 -0500

    Initial Commit

---
+++ FoundationModelsExample1/FoundationModelsExample1App.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample1App.swift
+//  FoundationModelsExample1
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsExample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelsExample1Tests/FoundationModelsExample1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample1Tests.swift
+//  FoundationModelsExample1Tests
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import Testing
+@testable import FoundationModelsExample1
+
+struct FoundationModelsExample1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
