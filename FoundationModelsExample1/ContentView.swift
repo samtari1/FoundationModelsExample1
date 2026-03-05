f736160e474e07ee62762cc9dbb7760810c10d31
samtari1 <samtariyang@gmail.com>
2026-03-05 10:28:44 -0500

    First Commit

--- FoundationModelsExample1/ContentView.swift
+++ FoundationModelsExample1/ContentView.swift
@@ -6,19 +6,36 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    @State private var response = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Send") {
+                let prompt = "Write a paragraph describing a unicorn"
+                let session = LanguageModelSession()
+                
+                if !session.isResponding {
+                    Task {
+                        do {
+                            let answer = try await session.respond(to: prompt)
+                            response = answer.content
+                        } catch {
+                            response = "Error accessing the model: \(error)"
+                        }
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Text(response)
+                .font(.system(size: 18))
+                .padding()
+            
+            Spacer()
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
